9536ad2b32e7561d95c30c0dec95a032f4d03c94
sherwinlu <blkse350h>
2018-07-26 15:57:51 -0700

    Fixing the schema being named incorrectly

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS `fitnesspal`.`message` (
+CREATE DATABASE IF NOT EXISTS `fitnesspaldb`;
+
+CREATE TABLE IF NOT EXISTS `fitnesspaldb`.`message` (
   `id` BIGINT NOT NULL,
   `username` VARCHAR(255) NOT NULL,
   `text` TEXT NOT NULL,
